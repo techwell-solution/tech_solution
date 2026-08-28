90f50b86894b54d32dfc877a616fd57fe528a19d
cataML <catelilungo@gmail.com>
2026-08-28 22:59:45 +0300

    Initial Django project setup

---
+++ techwellpj/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for techwellpj project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "techwellpj.settings")
+
+application = get_asgi_application()
